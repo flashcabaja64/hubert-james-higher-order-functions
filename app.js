6a751f1297a0c8551abfb5d8f7a82649dc20b51f
James Lee <jameswlee109@gmail.com>
2019-08-13 14:28:32 -0500

    second drill

---
+++ app.js
@@ -0,0 +1,31 @@
+// function repeat(fn, n){
+//     for(let i=0; i<n; i++){
+//         fn();
+//     }
+// }
+
+// console.log(repeat(function hello(){
+//     console.log( "Hello world")
+// }, 5));
+
+// console.log(repeat(function goodbye(){
+//     console.log("Goodbye world")
+// }, 5));
+
+// function filter(arr, fn) {
+//     let newArray=[]
+//     for(let i=0; i< arr.length; i++){
+//         if(fn(arr[i])){
+//             newArray.push(arr[i])
+//         }
+//     }
+//     return newArray;
+// }
+
+// const myNames = ['Rob', 'James', 'Alice', 'Rick']
+// const filteredNames = filter(myNames, function(name){
+//     return name[0] === 'R'
+// })
+
+// console.log(filteredNames)
+
